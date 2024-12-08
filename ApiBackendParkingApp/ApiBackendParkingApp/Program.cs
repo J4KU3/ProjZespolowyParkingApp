1b7ae34e3eb85d39f3b236ad0bbfeb7f66d6c438
PiotrW01 <piotr140401@gmail.com>
2024-12-08 17:17:30 +0100

    Add CORS development policy

--- ApiBackendParkingApp/ApiBackendParkingApp/Program.cs
+++ ApiBackendParkingApp/ApiBackendParkingApp/Program.cs
@@ -26,7 +26,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 
 builder.Services.AddScoped<IParkingLotRepository, ParkingLotRepository>();
@@ -41,6 +47,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("AllowLocalhost");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
